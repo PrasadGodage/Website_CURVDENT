f44ed2843d3d3bf899b3e26c8a4bf24637cf0666
pradyumnSoulsoft <pradyumn.soulsoftinfotech@gmail.com>
2023-09-04 15:38:52 +0530

    [CUR-2] moving all data to dev changes 01

---
+++ application/views/uiFolder/contact/contact.php
@@ -0,0 +1,81 @@
+<section class="section-padding" data-scroll-index="5" data-background="img/contact.png">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 mb-30">
+                    <h2 class="section-title" id="contact">Contact Us</h2>
+                    <hr class="line line-hr-left text-center">
+                </div>
+            </div>
+            <div class="row">
+                <!-- Contact Info -->
+                <div class="col-md-5 animate-box" data-animate-effect="fadeInUp">
+                    <div class="betty-contact-info2">
+                        <div class="feat-inner2"> <span class="icon et-phone"></span>
+                            <div class="feat-info2">
+                                <h5 class="text-light">Phone Text</h5>
+                                <h6 class="text-light">91 43 950 950</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="betty-contact-info2">
+                        <div class="feat-inner2"> <span class="icon et-map-pin"></span>
+                            <div class="feat-info2">
+                                <h5 class="text-light">Address</h5>
+                                <h6 class="text-light">Curvdent dental clinic , navin nagar road , Sangamner.</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="betty-contact-info2">
+                        <div class="feat-inner2"> <span class="icon et-notebook"></span>
+                            <div class="feat-info2">
+                                <h5 class="text-light">E-Mail</h5>
+                                <h6 class="text-light">info@curvedent.com</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="betty-contact-info2">
+                        <div class="feat-inner2"> <span class="icon et-clock"></span>
+                            <div class="feat-info2">
+                                <h5 class="text-light">Mon-Fri: 10 - 7</h5>
+                                <h6 class="text-light">Sunday: Closed</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Contact Form -->
+                <div class="col-md-7 animate-box" data-animate-effect="fadeInUp">
+                    <h6>Contact Form</h6>
+                    <form method="post" class="contact__form" action="mail.php">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="alert alert-success contact__msg" style="display: none" role="alert">
+                                    Your message was sent successfully.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <input name="name" type="text" placeholder="Name *" required>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <input name="email" type="email" placeholder="Email *" required>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <input name="phone" type="text" placeholder="Number *" required>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <input name="subject" type="text" placeholder="Subject *" required>
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <textarea name="message" id="message" cols="30" rows="4" placeholder="Message *"
+                                    required></textarea>
+                            </div>
+                            <div class="col-md-12">
+                                <input name="submit" type="submit" value="Send Message">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+</section>
