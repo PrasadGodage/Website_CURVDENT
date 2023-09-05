7aa7f1412ba695682a0c4192f836ea71c3a87e35
Lalit Meshram <lalitrmeshram@gmail.com>
2023-09-05 21:32:41 +0530

    changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,11 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$route['default_controller'] = 'uicontroller';
+$route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 
 //--------------- Controller UI --------------------------------------
 
-$route['home']='UIController/home';
+$route['home']='welcome/home';
