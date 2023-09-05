f7d61ba49940bf0ed882cc71f763f9b2426a22bc
Lalit Meshram <lalitrmeshram@gmail.com>
2023-09-05 21:09:21 +0530

    [CUR-02] to develop

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,11 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$route['default_controller'] = 'ui/UIController';
+$route['default_controller'] = 'UIController';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 
 //--------------- Controller UI --------------------------------------
 
-$route['home']='ui/UIController/home';
+$route['home']='UIController/home';
