a6a042f2d49776f6b164c332f137f20e8a9abe11
Lalit Meshram <lalitrmeshram@gmail.com>
2023-09-05 20:48:50 +0530

    [CUR-02] to develop

---
+++ application/views/ui/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>Curvdent Clinic</title>
+    <link rel="shortcut icon" href="<?php echo base_url() . 'uiAssets/img/logo-light.png'; ?>" />
+    <link rel="stylesheet" href="<?php echo base_url() . 'uiAssets/css/plugins.css'; ?>" />
+    <link rel="stylesheet" href="<?php echo base_url() . 'uiAssets/css/style.css'; ?>" />
+</head>
+
+<body>
+    
