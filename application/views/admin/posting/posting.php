f8eba79591dcc9781b504157fb39fa1cb4bce07c
pradyumnSoulsoft <pradyumn.soulsoftinfotech@gmail.com>
2023-09-09 13:48:34 +0530

    [CUR-7] Changes 06

--- application/views/admin/posting/posting.php
+++ application/views/admin/posting/posting.php
@@ -3,12 +3,12 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            Add Post
+            Client
         </h1>
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
             <!--<li class="breadcrumb-item"><a href="#">Master</a></li>-->
-            <li class="breadcrumb-item active">Posting</li>
+            <li class="breadcrumb-item active">Client Master</li>
         </ol>
     </section>
 
@@ -18,7 +18,7 @@
         <!-- Default box -->
         <div class="box">
             <div class="box-header with-border">
-                <h3 class="box-title">Add Post</h3>
+                <h3 class="box-title">Client List</h3>
 
                 <ul class="box-controls pull-right">
                     <li><a class="box-btn-close" href="#"></a></li>
@@ -32,23 +32,21 @@
                     <div class="box">
                         <div class="box-header with-border box-controls pull-right">
 
-                            <div class="box-controls pull-right">
-                                <button type="button" class="btn btn-primary" id="addPostBtn">Add Post</button>
+                            <div class="box-controls pull-left">
+                            <!--<button id="row-count" class="btn btn-xs btn-primary">Row count</button>-->
+                            <button type="button" class="btn btn-primary" id="addClientBtn">Add Client</button>
                             </div>
                         </div>
-                        <!-- /.box-header -->
-                      
-                        <div class="box">
-                            <div class="box-header with-border">
+                        <div class="box-header with-border">
                             <!-- <h3 class="box-title">Data Table With Full Features</h3> -->
                                 <div class="box-controls pull-right">
                                     <button id="row-count" class="btn btn-xs btn-danger">Row count</button>
                                 </div>
                             </div>
-                                <!-- /.box-header -->
-                                <div class="box-body">
-                                    <div class="table-responsive">
-                                        <table id="postTable" class="table table-bordered table-striped">
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <div class="table-responsive">
+                            <table id="postTable" class="table table-bordered table-striped">
                                             <thead>
                                                 <tr>
                                                     <th>Article Title</th>
@@ -77,11 +75,8 @@
                                                 </tr>
                                             </tfoot>
                                         </table>
-                                    </div>
-                                </div>
-                                <!-- /.box-body -->
+                            </div>
                         </div>
-
                         <!-- /.box-body -->
                     </div>
                     <!-- /.box -->
