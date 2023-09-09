4142182056ee89d8ab69d79d021d41f6a9ee94de
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-09-09 14:28:16 +0530

    [CUR-8] changes on develop-v1 to develop

--- application/views/admin/category/category.php
+++ application/views/admin/category/category.php
@@ -66,7 +66,7 @@
                         <!-- /.box-body -->
 
 
-                        <div class="box-body">
+            <div class="box-body">
 				<div class="table-responsive">         
 				  <table id="example1" class="table table-bordered table-striped">
 					<thead>
