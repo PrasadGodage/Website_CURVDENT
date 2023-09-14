08906ff5d82595339510c64e167f8d182f25969a
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-09-14 10:53:48 +0530

    [CUR-9] changes on develop-v1 to develop

--- application/controllers/admin/AdminController.php
+++ application/controllers/admin/AdminController.php
@@ -21,19 +21,20 @@ class AdminController extends CI_Controller {
         $this->load->view('header');
         $this->load->view('admin/sidebar/side_bar');
         $this->load->view('admin/posting/posting');
+        $this->load->view('admin/posting/modal/add_post');
         $this->load->view('footer');
         $this->load->view('admin/posting/posting_js');
         $this->load->view('htmlend');
     }
     
-    public function addPost() {
-        $this->load->view('header');
-        $this->load->view('admin/sidebar/side_bar');
-        $this->load->view('admin/posting/add_post');
-        $this->load->view('footer');
-        $this->load->view('admin/posting/add_post_js');
-        $this->load->view('htmlend');
-    }
+    // public function addPost() {
+    //     $this->load->view('header');
+    //     $this->load->view('admin/sidebar/side_bar');
+    //     $this->load->view('admin/posting/add_post');
+    //     $this->load->view('footer');
+    //     $this->load->view('admin/posting/add_post_js');
+    //     $this->load->view('htmlend');
+    // }
     
     public function category() {
         $this->load->view('header');
