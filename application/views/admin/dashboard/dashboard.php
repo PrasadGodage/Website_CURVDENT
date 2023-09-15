2e3626110dea85c01e0f3ac52eaa8496d6b6149c
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-09-15 09:28:01 +0530

    [CUR-3] changes on develop-v1 to develop

--- application/views/admin/dashboard/dashboard.php
+++ application/views/admin/dashboard/dashboard.php
@@ -15,7 +15,7 @@
     <section class="content">
 	  <div class="row">
 	    <div class="col">
-            <div class="alert alert-pale-info alert-dismissible fade show callout bg-pale-info" role="alert">
+            <div class="alert alert-pale-info alert-dismissible fade show callout" role="alert">
                 Logged In Successfully...               
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
                      <span aria-hidden="true">×</span>
