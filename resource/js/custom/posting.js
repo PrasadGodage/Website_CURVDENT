ee249a47a6104a7ca4a66b30df30f517019ffaa2
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-09-15 17:02:32 +0530

    [CUR-9] changes on develop-v1 to develop

--- resource/js/custom/posting.js
+++ resource/js/custom/posting.js
@@ -1,9 +1,60 @@
 let categoryList = new Map();
 let postList = new Map();
 
+//Submit Category Btn script
 
+$('#addPostForm').on('submit', function (e) {
 
+    e.preventDefault();
 
+    var returnVal = $("#addPostForm").valid();
+    var formdata = new FormData(this);
+    if (returnVal) {
+        $.ajax({
+
+            url: ebase_url+'posting_api',
+
+            type: 'POST',
+
+            headers: {
+                "Authorization": etoken
+            },
+
+            data: formdata,
+
+            cache: false,
+
+            contentType: false,
+
+            processData: false,
+
+            dataType: 'json',
+
+            success: function (response) {
+                if (response.status == 200) {
+                    $('#addPostModal').modal('toggle');
+
+                    let id=response.data.id;
+                  
+                 if(postList.has(id)){
+                    postList.delete(id);   
+                 }
+                 postList.set(id, response.data);
+                 setPostList(postList);
+
+                    swal("Good job!", response.msg, "success");
+                    $(location).attr('href',ebase_url+'posting');
+                } else {
+
+                    swal("Good job!", response.msg, "error");
+
+                }
+
+            }
+
+        });
+    }
+});
 
 
 //Add Category Btn script -----------------------------------------------------------------
@@ -16,10 +67,75 @@ $('#addPostBtn').click(function () {
 
 
 
+// get category data
+function getPostList() {
+    $.ajax({
+
+        url: ebase_url+'posting_api',
+
+        type: 'GET',
+
+        async:false,
+
+        headers: {
+            "Authorization": etoken
+        },
+
+        dataType: 'json',
+
+        success: function (response) {
+        
+
+            if (response.status == 200) {
+
+                if (response.data.lenght != 0) {
+                    for (var i = 0; i < response.data.length; i++) {
+                        postList.set(response.data[i].id, response.data[i]);
+                    }
+                    
+                }
+                setPostList(postList);
+                console.log(postList);
+            }
+
+        }
+        
+    });
+}
+getCategoryList();
 
 
+function setPostList(list) {
 
-
+    $('#postTable').dataTable().fnDestroy();
+    $('#postList').empty();
+    var tblData = '';
+    var index=1;
+    
+    for (let k of list.keys()) {
+        
+        let post = list.get(k);
+    
+        tblData += `
+        <tr>
+                <td>` + index + `</td>
+                <td>` + post.title + `</td>
+                <td>` + post.featured + `</td>
+                <td>` + post.choice + `</td>
+                <td>` + post.thread + `</td>
+                <td>` + post.category_name + `</td>
+                <td>` + post.is_active + `</td>
+                <td>` + post.date + `</td>
+                <td> <a href="#" onclick="updatePostDetails(${post.id})" ><i class="mdi mdi-tooltip-edit" style="font-size: 20px;"></i></a>             
+                </td>
+                
+        </tr>`;
+        index++;
+    }
+    
+    $('#postList').html(tblData);
+    $('#postTable').DataTable();
+    }
 
 
 
