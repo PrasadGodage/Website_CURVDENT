2289780fc912962d8fb9d840de258544383da5f6
Soullaxman <soulsoft.laxman@gmail.com>
2023-09-18 14:19:40 +0530

    [CUR-11] changes on develop

--- application/views/ui/blogs/blog.php
+++ application/views/ui/blogs/blog.php
@@ -57,11 +57,11 @@
                             <div class="item">
                                 <div class="media-body">
                                     <div class="row">
-                                        <div class="col-sm-4"><button type="button" class="btn btn-primary">Blog</button></div>
+                                        <div class="col-sm-4"><button type="button" class="btn btn-warning">Blog</button></div>
                                         <div class="col-sm-4"></div>
                                         <div class="col-sm-4"><i class="fa fa-calendar" aria-hidden="true"></i>May 22, 2023</a></div>
-                                        <div class="col-lg-12">
-                                            <h4 style="color:red;">The Definitive Story of How Marvel Studios Created the Marvel Cinematic Universe</h4>
+                                        <div class="col-md-12">
+                                            <h2 style="color:red;">The Definitive Story of How Marvel Studios Created the Marvel Cinematic Universe</h2>
                                         </div>
                                         <div class="col-md-12">
                                             <p>Get your first look at The Story of Marvel Studios: The Making of the Marvel Cinematic Universe from…</p>
@@ -72,7 +72,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-6 p-4">
+                            <div class="col-md-5 p-4">
                                 <div class="item">
                                     <div class="position-re o-hidden"><img src="<?php echo base_url() . 'uiAssets/'; ?>img/Antiaging.jpg" alt=""
                                             style="height: 185px;">
@@ -80,10 +80,21 @@
 
                                 </div>
                             </div>
-                            <div class="col-md-6 p-4">
+                            <div class="col-md-7 p-4">
                                 <div class="item">
-                                    <div class="position-re o-hidden"> <img src="<?php echo base_url() . 'uiAssets/'; ?>img/Non surgical face.jpg" alt=""
-                                            style="height: 185px;"> </div>
+                                    <div class="media-body">
+                                      <div class="row">
+                                        <div class="col-sm-4"><button type="button" class="btn btn-warning">Blog</button></div>
+                                        <div class="col-sm-4"></div>
+                                        <div class="col-sm-4"><i class="fa fa-calendar" aria-hidden="true"></i>May 22, 2023</a></div>
+                                        <div class="col-md-12">
+                                            <h2 style="color:red;">The Definitive Story of How Marvel Studios Created the Marvel Cinematic Universe</h2>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <p>Get your first look at The Story of Marvel Studios: The Making of the Marvel Cinematic Universe from…</p>
+                                        </div>
+                                      </div>  
+                                   </div>
 
                                 </div>
                             </div>
