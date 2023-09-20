f843a53d2f8b7d20f6a574c7f64d8dca8aa87a29
Soullaxman <soulsoft.laxman@gmail.com>
2023-09-20 08:59:19 +0530

    [CUR-11] changes on develop

--- application/views/ui/blogs/blog.php
+++ application/views/ui/blogs/blog.php
@@ -269,22 +269,22 @@
                                     <li><a href="services">Skin Lightening</a></li>
                                     <li><a href="services">Treatment</a></li>
                                     <li><a href="services">Pigmentation Treatment</a></li>
-                                    <li><a href="services">Melasma Treatment</a></li>
-                                    <li><a href="services">Dark Circle Treatment</a></li>
-                                    <li><a href="services">Dull Skin Treatment</a></li>
-                                    <li><a href="services">Signature Facial</a></li>
-                                    <li><a href="services">Stretch Marks Removal</a></li>
-                                    <li><a href="services">Treatment</a></li>
-                                    <li><a href="services">Anti-Aging Treatment</a></li>
-                                    <li><a href="services">Botox Treatment</a></li>
-                                    <li><a href="services">Filters Treatment</a></li>
-                                    <li><a href="services">Skin Tightiening</a></li>
-                                    <li><a href="services">Permanent Tatto</a></li>
-                                    <li><a href="services">Removal</a></li>
-                                    <li><a href="services">Mole Removal</a></li>
-                                    <li><a href="services">Wart Removal</a></li>
-                                    <li><a href="services">Skin Tag Removal</a></li>
-                                    <li><a href="services">Liquid Rhinoplasty</a></li>
+                                    <!-- <li><a href="services">Melasma Treatment</a></li> -->
+                                    <!-- <li><a href="services">Dark Circle Treatment</a></li> -->
+                                    <!-- <li><a href="services">Dull Skin Treatment</a></li> -->
+                                    <!-- <li><a href="services">Signature Facial</a></li> -->
+                                    <!-- <li><a href="services">Stretch Marks Removal</a></li> -->
+                                    <!-- <li><a href="services">Treatment</a></li> -->
+                                    <!-- <li><a href="services">Anti-Aging Treatment</a></li> -->
+                                    <!-- <li><a href="services">Botox Treatment</a></li> -->
+                                    <!-- <li><a href="services">Filters Treatment</a></li> -->
+                                    <!-- <li><a href="services">Skin Tightiening</a></li> -->
+                                    <!-- <li><a href="services">Permanent Tatto</a></li> -->
+                                    <!-- <li><a href="services">Removal</a></li> -->
+                                    <!-- <li><a href="services">Mole Removal</a></li> -->
+                                    <!-- <li><a href="services">Wart Removal</a></li> -->
+                                    <!-- <li><a href="services">Skin Tag Removal</a></li> -->
+                                    <!-- <li><a href="services">Liquid Rhinoplasty</a></li> -->
                                 </ul>
                             </div>
                         </div>
