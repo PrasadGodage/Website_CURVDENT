f5232301da1dd28cf36ad9a824de3630ca115d77
pradyumnSoulsoft <pradyumn.soulsoftinfotech@gmail.com>
2023-09-21 14:01:03 +0530

    [CUR-13] Changes 01

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -41,7 +41,7 @@ class Welcome extends CI_Controller {
         // $this->load->view('sidebar/side_bar');
         $this->load->view('ui/blogs/blog');
         $this->load->view('ui/footer');
-        // $this->load->view('ui/blogs/blog_js');
+        $this->load->view('ui/blogs/blog_js');
     }
 	
 	public function blogPage() {        
