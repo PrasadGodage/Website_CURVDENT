db007efa53a20212bc4688479abdad678d6ee722
pradyumnSoulsoft <pradyumn.soulsoftinfotech@gmail.com>
2023-09-22 11:10:30 +0530

    [CUR-14] Changes 01

---
+++ application/controllers/api/CategoryUiController.php
@@ -0,0 +1,129 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class CategoryUiController extends REST_Controller {
+
+    public function __construct() {
+
+        parent::__construct();
+        // $this->load->library('Authorization_Token'); 
+        $this->load->model('CategoryModel','category');
+        
+    }
+
+    public function category_get($id = 0) {
+        $response = [];
+
+            
+        try {
+            //Authentication
+            // $headers = $this->input->request_headers();
+
+            // if (isset($headers['Authorization'])) {
+            //     $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+                // if ($decodedToken['status'])
+                // {
+                    $data = $this->category->get_category($id);
+                    if (!empty($data)) {
+                        $response['data'] = $data;
+                        $response['msg'] = 'All Data Fetch successfully!';
+                        $response['status'] = 200;
+                        $this->response($response, REST_Controller::HTTP_OK);
+                    } else {
+                        $response['msg'] = 'Data not Found!';
+                        $response['status'] = 404;
+                        $this->response($response, REST_Controller::HTTP_OK);
+                    }
+                    //     }else {
+                    //     $this->response($decodedToken);
+                    // }
+                    // }else {
+                    //     $this->response(['Authentication failed'], REST_Controller::HTTP_OK);
+                    // }
+
+            
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+    }
+
+    public function category_post() { 
+        $response = [];
+        $data['category_name'] = $this->post('category_name');
+        $data['slug'] = $this->post('slug');
+        $data['is_active'] = $this->post('is_active');
+
+        $id = $this->post('id');
+        
+        //Authentication
+        // $headers = $this->input->request_headers();
+
+        // if (isset($headers['Authorization'])) {
+            // $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+            // if ($decodedToken['status'])
+            // {
+                      
+            if (empty($id)) {
+                if($this->category->find_category($data['category_name'])){
+                   $category_id = $this->category->insert_category($data);
+
+            if (!empty($category_id)) {
+                $restData = $this->category->get_category($category_id);
+                $response['msg'] = 'Category created successfully!';
+                $response['data'] = $restData;
+                $response['status'] = 200;
+                $this->response($response, REST_Controller::HTTP_OK);
+            } else {
+                $response['msg'] = 'Bad Request!';
+                $response['id'] = $id;
+                $response['status'] = 400;
+                $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+            } 
+                }else{
+                    $response['msg'] = 'Duplicate Entry!';
+                $response['status'] = 400;
+                $this->response($response, REST_Controller::HTTP_OK);
+                }
+                
+        } else {
+            $result=$this->category->get_category($id);
+            if (!empty($result)) {
+                if($this->category->find_category($data['category_name']) || $result['category_name']==$data['category_name']){
+                    
+                $status = $this->category->update_category($id, $data);
+                if ($status) {
+                    $restData = $this->category->get_category($id);
+                    $response['msg'] = 'Category updated successfully!';
+                    $response['data'] = $restData;
+                    $response['status'] = 200;
+                    $this->response($response, REST_Controller::HTTP_OK);
+                }
+            }else{
+                $response['msg'] = 'Duplicate Entry!';
+                $response['status'] = 400;
+                $this->response($response, REST_Controller::HTTP_OK);
+            }
+            } else {
+                $response['msg'] = 'Data not found!';
+                $response['id'] = $id;
+                $response['status'] = 400;
+                $this->response($response, REST_Controller::HTTP_OK);
+            }
+        }
+      
+        //     }else {
+        //     $this->response($decodedToken);
+        // }
+        // }else {
+        //     $this->response(['Authentication failed'], REST_Controller::HTTP_OK);
+        // }
+        
+    }
+
+
+
+
+}    
