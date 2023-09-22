5f773d5a02e1e570d45e0215dcead3e5017747f0
pradyumnSoulsoft <pradyumn.soulsoftinfotech@gmail.com>
2023-09-22 14:15:36 +0530

    [CUR-13] Changes 07

--- application/views/ui/blogs/blog.php
+++ application/views/ui/blogs/blog.php
@@ -39,10 +39,10 @@
         </div>
     </header>
 <!-- section -->
-<!--
 <section class="section-padding">
     <div class="container">
         <div class="row">
+            <!-- Content -->
             <div class="col-md-8 animate-box" data-animate-effect="fadeInUp">
                 <div class="main_title2"><h6 style=" font-weight:bold;">All News About Blog</h6></div>
                 <div class="row">
@@ -66,10 +66,13 @@
                                     </div>
                                     <div class="col-sm-4"><i class="fa fa-calendar" aria-hidden="true" href="blog_page"></i>May 22, 2023</a></div>
                                     <div class="col-md-12" id="heading1">
-                                        
+                                        <!-- <h5 style="color:red;" href="blog_page" id="heading1"> -->
+                                        <!-- The Definitive Story of How Marvel Studios Created the Marvel Cinematic Universe -->
+                                    <!-- </h5> -->
                                     </div>    
                                     <div class="col-md-12" href="blog_page" id="p1">
-
+                                        <!-- <p id="paragraph1"></p> -->
+                                        <!-- <p>Get your first look at The Story of Marvel Studios: The Making of the Marvel Cinematic Universe from…</p> -->
                                     </div>
                                 </div> 
                             </div>
@@ -80,6 +83,7 @@
                     <div class="col-md-5 p-4">
                         <div class="item">
                             <div class="position-re o-hidden">
+                                <a href="blog_page"><img src= "<?php echo base_url() . 'uiAssets/img/pimple.jpg'; ?>" alt=""
                                     style="height: 185px;">
                                 </a>
                             </div>
@@ -110,6 +114,7 @@
                     <div class="col-md-5 p-4">
                         <div class="item">
                             <div class="position-re o-hidden">
+                                <a href="blog_page"><img src= "<?php echo base_url() . 'uiAssets/img/pimple.jpg'; ?>" alt=""
                                     style="height: 185px;">
                                 </a>
                             </div>
@@ -142,6 +147,7 @@
                     <div class="col-md-5 p-4">
                         <div class="item">
                             <div class="position-re o-hidden">
+                                <a href="blog_page"><img src= "<?php echo base_url() . 'uiAssets/img/pimple.jpg'; ?>" alt=""
                                     style="height: 185px;">
                                 </a>
                             </div>
@@ -172,6 +178,7 @@
                     <div class="col-md-5 p-4">
                         <div class="item">
                             <div class="position-re o-hidden">
+                                <a href="blog_page"><img src= "<?php echo base_url() . 'uiAssets/img/pimple.jpg'; ?>" alt=""
                                     style="height: 185px;">
                                 </a>
                             </div>
@@ -199,6 +206,7 @@
                     </div>
                 </div>
             </div>
+            <!-- Sidebar -->
             <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
                 <div class="main_title2"><h6 style=" font-weight:bold;">Most Popular News</h6></div>
                 <div class="row">
@@ -235,6 +243,7 @@
                                     <div class="card w-100 float-left">
                                         <div class="card-header bg-primary p-0">
                                             <div class="item">
+                                                <div class="position-re o-hidden"><img src="<?php echo base_url() . 'uiAssets/img/Skin glowing treatment.jpg'; ?>" alt="..."></div>
                                             </div>
                                             <div class="card-body bg-dark">
                                                 <h1 class="text-white text-center h4">-- Skin/Hair Treatments --</h1>
@@ -255,6 +264,7 @@
                                     <div class="card w-100 float-left">
                                         <div class="card-header bg-primary p-0">
                                             <div class="item">
+                                                <div class="position-re o-hidden"><img src="<?php echo base_url() . 'uiAssets/img/Skin lightening treatment.jpg'; ?>" alt="..."></div>
                                             </div>
                                             <div class="card-body bg-dark">
                                                 <h1 class="text-white text-center h4">-- Skin/Hair Treatments --</h1>
@@ -273,7 +283,7 @@
                                 </div>
                             </div>
                             </div>
-                             <div class="row">
+                            <!-- <div class="row">
                             <div class="col-4">
                                 <div> 
                                     <button class="" type="button" data-target="#sliderid" data-slide="prev"><span class="bg-danger p-2">Back</span></button>
@@ -285,7 +295,7 @@
                                     <button class="" type="button" data-target="#sliderid" data-slide="next"><span class="bg-success p-2">Next</span></button>
                                 </div>
                             </div>    
-                            </div> 
+                            </div> -->
                             
                         </div>
                     </div>
@@ -297,7 +307,7 @@
                         <div class="betty-sidebar-block-content">
                             <h6>-- Skin/Hair Treatments --</h6>
                                 <ul class="ul1" id="uiList">
-                                    <li><a href="services">Laser Hair Removal</a></li>
+                                <!-- <li><a href="services">Laser Hair Removal</a></li>
                                     <li><a href="services">Pimple/Acne Scar</a></li>
                                     <li><a href="services">Pimple Treatment</a></li>
                                     <li><a href="services">Removal</a></li>
@@ -320,7 +330,7 @@
                                     <li><a href="services">Mole Removal</a></li>
                                     <li><a href="services">Wart Removal</a></li>
                                     <li><a href="services">Skin Tag Removal</a></li>
-                                    <li><a href="services">Liquid Rhinoplasty</a></li>
+                                    <li><a href="services">Liquid Rhinoplasty</a></li> -->
                                 </ul>
                         </div>
                     </div>
@@ -328,51 +338,6 @@
             </div>
         </div>
     </div>
-</section> -->
-
-
-<section class="news_area p_100">
-   <div class="container">
-      <div class="row">
-         <div class="col-lg-8">
-            <div class="main_title2">
-               <h2>All News </h2>
-            </div>
-            <div class="latest_news">
-                  <div class="media">
-                     <div class="d-flex">
-                        <img class="img-fluid" src="" alt="">
-                     </div>
-                     <div class="media-body">
-                        <div class="choice_text">
-                           <div class="date">
-                              <a class="gad_btn" href="#"></a>
-                              <a href="#" class="float-right"><i class="fa fa-calendar" aria-hidden="true">
-                              </a>
-                           </div>
-                           <a href="#">
-                              <h4>abcd</h4>
-                           </a>
-                           <p>abcd</p>
-                        </div>
-                     </div>
-                  </div>
-            </div>
-
-            <!-- Pagination -->
-            <div class="row mt-5">
-               <div class="col">
-                  <nav aria-label="Page navigation example">
-                  </nav>
-               </div> 
-            </div>
-            <!-- End of Pagination -->
-            
-         </div>
-         <!-- ================Sidebar================== -->
-         <!-- ================End of Sidebar================== -->
-      </div>
-   </div>
 </section>
 
     <!-- Style for Title -->
