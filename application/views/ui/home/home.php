a49826984025aff31a2196bfb65fb1d6f9228161
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-09-26 18:10:29 +0530

    [CUR-15] changes on develop-v1 to develop

--- application/views/ui/home/home.php
+++ application/views/ui/home/home.php
@@ -151,8 +151,8 @@
                 <div class="col-md-1 mb-20 animate-box" data-animate-effect="fadeInUp"></div>
             </div>
             <div class="row">
-                <div class="col-md-4 mb-20 animate-box" data-animate-effect="fadeInUp"></div>
-                <div class="col-md-8 mb-20 animate-box" data-animate-effect="fadeInUp">
+                <div class="col-md-5 mb-20 animate-box" data-animate-effect="fadeInUp"></div>
+                <div class="col-md-7 mb-20 animate-box" data-animate-effect="fadeInUp">
                      <a  href="blog_page">
                         <button type="button" class="btn btn-warning" style="display: flex; justify-content: center; align-items: center;">Read More</button>
                     </a>
