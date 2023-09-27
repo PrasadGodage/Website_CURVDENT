dffc12f438a83478168130d608beb546be09931d
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-09-27 18:25:56 +0530

    [CUR-20] changes on develop-v1 to develop

--- application/views/admin/subscriber/subscriber.php
+++ application/views/admin/subscriber/subscriber.php
@@ -38,7 +38,7 @@
                         <!-- /.box-header -->
                         <div class="box-body">
                             <div class="table-responsive">
-                                <table id="categoryTable" class="table table-bordered table-striped">
+                                <table id="subscriberTable" class="table table-bordered table-striped">
                                     <thead>
                                         <tr>
                                             <th>Sr.No</th>
@@ -46,7 +46,7 @@
                                             <th>Action</th>
                                         </tr>
                                     </thead>
-                                    <tbody id="categoryList">
+                                    <tbody id="subscriberList">
                                         
                                     </tbody>
                                     <tfoot>
---
+++ resource/js/custom/subscriber.js
@@ -0,0 +1,68 @@
+let subscriberList = new Map();
+
+// get posting data
+function getSubscriberList() {
+    $.ajax({
+
+        url: ebase_url+'newsletter_api',
+
+        type: 'GET',
+
+        async:false,
+
+        headers: {
+            "Authorization": etoken
+        },
+
+        dataType: 'json',
+
+        success: function (response) {
+        
+
+            if (response.status == 200) {
+
+                if (response.data.length != 0) {
+                    for (var i = 0; i < response.data.length; i++) {
+                        subscriberList.set(response.data[i].id, response.data[i]);
+                    }
+                    
+                }
+                setSubscriberList(subscriberList);
+                console.log(subscriberList);
+            }
+
+        }
+        
+    });
+}
+getSubscriberList();
+
+//-------------
+
+function setSubscriberList(list) {
+
+    $('#subscriberTable').dataTable().fnDestroy();
+    $('#subscriberList').empty();
+    var tblData = '';
+    var index=1;
+    
+    for (let k of list.keys()) {
+        
+        let subscriber = list.get(k);
+    
+        tblData += `
+        <tr>
+                <td>` + index + `</td>
+                <td>` + subscriber.email + `</td>
+                <td>` + subscriber.is_active + `</td>
+                <td> <a href="#" onclick="updatesubscriberDetails(${subscriber.id})" ><i class="mdi mdi-tooltip-edit" style="font-size: 20px;"></i></a>
+                <a href="#" onclick="deletesubscriberDetails(${subscriber.id})"><i class="mdi mdi-delete-circle" style="font-size: 20px;"></i></a>                          
+                </td>
+                
+        </tr>`;
+        index++;
+    }
+    
+    $('#subscriberList').html(tblData);
+    $('#subscriberTable').DataTable();
+    }
