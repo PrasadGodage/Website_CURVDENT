897fb222582d60a5798a5d4f071d21a0dded618c
Soullaxman <soulsoft.laxman@gmail.com>
2023-09-29 13:27:18 +0530

    [CUR-22] changes on develop

--- resource/js/custom/blog.js
+++ resource/js/custom/blog.js
@@ -117,7 +117,7 @@ function setPostList(postList) {
             </div>
         </div>`;
         
-        setPaginator(data1);   
+        // setPaginator(data1);   
     }
         data2 += '<div class="main_title2"><h6 style="font-weight:bold;">Most Popular News</h6></div>';
         let firstKey = null;
@@ -355,66 +355,66 @@ $(function () {
 
  //Paginator script
 
-function setPaginator(data){
-   //Get total number of pages
-const listArray = []
-for (let i=0; i<40; i++) {
-    var a = data;
-    listArray.push(a)
-}
-console.log(listArray);
+// function setPaginator(data){
+//    //Get total number of pages
+// const listArray = []
+// for (let i=0; i<40; i++) {
+//     var a = data;
+//     listArray.push(a)
+// }
+// // console.log(listArray)
 
-// State
-// Number of products
-const numberOfItems = listArray.length
-const numberPerPage = 5
-const currentPage = 1
+// // State
+// // Number of products
+// const numberOfItems = listArray.length
+// const numberPerPage = 5
+// const currentPage = 1
 
-// Number of pages
-const numberOfPages = Math.ceil(numberOfItems/numberPerPage)
-buildPage(1)
-    buildPagination(currentPage)
+// // Number of pages
+// const numberOfPages = Math.ceil(numberOfItems/numberPerPage)
+// buildPage(1)
+//     buildPagination(currentPage)
 
-    $('.paginator').on('click', 'button', function() {
-        var clickedPage = parseInt($(this).val())
-        buildPagination(clickedPage)
-        console.log(`Page clicked on ${clickedPage}`)
-        buildPage(clickedPage)
-    });
+//     $('.paginator').on('click', 'button', function() {
+//         var clickedPage = parseInt($(this).val())
+//         buildPagination(clickedPage)
+//         console.log(`Page clicked on ${clickedPage}`)
+//         buildPage(clickedPage)
+//     });
 
-}
+// }
 
 
 
-function accomodatePage(clickedPage) {
-    if (clickedPage <= 1) { return clickedPage + 1}
-    if (clickedPage >= numberOfPages) { return clickedPage -1}
-    return clickedPage
-}
+// function accomodatePage(clickedPage) {
+//     if (clickedPage <= 1) { return clickedPage + 1}
+//     if (clickedPage >= numberOfPages) { return clickedPage -1}
+//     return clickedPage
+// }
 
-function buildPagination(clickedPage) {
-    $('.paginator').empty()
-    const currPageNum = accomodatePage(clickedPage)
-    if (numberOfPages >= 3) {
-        for (let i=-1; i<2; i++) {
-            $('.paginator').append(`<button class="btn btn-primary" value="${currPageNum+i}">${currPageNum+i}</button>`)
-        }
-    } else {
-        for (let i=0; i<numberOfPages; i++) {
-            $('.paginator').append(`<button class="btn btn-primary" value="${i+1}">${i+1}</button>`)
-        }
-    }
-}
+// function buildPagination(clickedPage) {
+//     $('.paginator').empty()
+//     const currPageNum = accomodatePage(clickedPage)
+//     if (numberOfPages >= 3) {
+//         for (let i=-1; i<2; i++) {
+//             $('.paginator').append(`<button class="btn btn-primary" value="${currPageNum+i}">${currPageNum+i}</button>`)
+//         }
+//     } else {
+//         for (let i=0; i<numberOfPages; i++) {
+//             $('.paginator').append(`<button class="btn btn-primary" value="${i+1}">${i+1}</button>`)
+//         }
+//     }
+// }
 
-function buildPage(currPage) {
-    const trimStart = (currPage-1)*numberPerPage
-    const trimEnd = trimStart + numberPerPage
-    console.log(trimStart, trimEnd)
-    console.log(listArray.slice(trimStart, trimEnd))
-    $('.content').empty().append(listArray.slice(trimStart, trimEnd))
-    // $('.grid-uniform').empty().append(listArray.slice(trimStart, trimEnd));
-}
+// function buildPage(currPage) {
+//     const trimStart = (currPage-1)*numberPerPage
+//     const trimEnd = trimStart + numberPerPage
+//     console.log(trimStart, trimEnd)
+//     console.log(listArray.slice(trimStart, trimEnd))
+//     $('.content').empty().append(listArray.slice(trimStart, trimEnd))
+//     // $('.grid-uniform').empty().append(listArray.slice(trimStart, trimEnd));
+// }
 
-$(document).ready(function(){
+// $(document).ready(function(){
     
-}); //End Paginator script
+// }); //End Paginator script
