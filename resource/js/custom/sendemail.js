da3ebd996800122f0c4c234e9b8fdd53d04f9724
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-09-30 17:54:46 +0530

    [CUR-28] changes on develop-v1 to develop

--- resource/js/custom/sendemail.js
+++ resource/js/custom/sendemail.js
@@ -1,21 +1,12 @@
-let subscriberList = new Map();
+// let subscriberList = new Map();
 
-//Add modal send Btn script -----------------------------------------------------------------
-// $('#newsletterTable').on('click', '.send-button', function (e) {
-//     $('#addSendEmailModal').modal('toggle');
-//     $("#addSendEmailForm").trigger("reset");
-//     $('#id').val('');
-//     $('.error').text('');
-// });
-
-
-// //Add Subscriber Btn script -----------------------------------------------------------------
-// $('#addSubscriberBtn').click(function () {
-//     $('#addSendEmailModal').modal('toggle');
-//     $("#addSubscriberForm").trigger("reset");
-//     $('#id').val('');
-//     $('.error').text('');
-// });
+//Add Subscriber Btn script -----------------------------------------------------------------
+$('#addSubscriberBtn').click(function () {
+    $('#addSendEmailModal').modal('toggle');
+    $("#addSubscriberForm").trigger("reset");
+    $('#id').val('');
+    $('.error').text('');
+});
 
 //------------- show table data ----------------------------
 
@@ -47,6 +38,66 @@ function setSubscriberList(list) {
     }
 
 
+
+$('#addSendEmailForm').on('submit', function (e) {
+
+    e.preventDefault();
+
+//     var returnVal = $("#addSendEmailForm").valid();
+//     var formdata = new FormData(this);
+//     console.log(formdata);
+//     if (returnVal) {
+//         $.ajax({
+
+//             url: ebase_url+'postNewsletter_api',
+
+//             type: 'POST',
+
+//             headers: {
+//                 "Authorization": etoken
+//             },
+
+//             data: formdata,
+
+//             cache: false,
+
+//             contentType: false,
+
+//             processData: false,
+
+//             dataType: 'json',
+
+//             success: function (response) {
+
+//                 console.log(response);
+
+//                 if (response.status == 200) {
+//                     $('#addSendEmailForm').modal('toggle');
+
+//                     let id=response.data.id;
+                  
+//                  if(postList.has(id)){
+//                     postList.delete(id);   
+//                  }
+//                  postList.set(id, response.data);
+//                  setPostList(postList);
+
+//                     swal("Good job!", response.msg, "success");
+//                     $(location).attr('href',ebase_url+'posting');
+//                 } else {
+
+//                     swal("Good job!", response.msg, "error");
+
+//                 }
+
+//             }
+
+//         });
+//     }
+});
+
+
+
 // get posting data
 function getSubscriberList() {
     $.ajax({
@@ -71,7 +122,6 @@ function getSubscriberList() {
                 if (response.data.length != 0) {
                     for (var i = 0; i < response.data.length; i++) {
                         subscriberList.set(response.data[i].id, response.data[i]);
-                    // sendEmail(response.data[0]);
                     }
                     
                 }
@@ -84,3 +134,4 @@ function getSubscriberList() {
     });
 }
 getSubscriberList();
+
