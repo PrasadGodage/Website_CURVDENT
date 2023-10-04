e073e6ecb50e78c2b9de7c42623008eb4a1c070d
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-10-04 11:23:46 +0530

    [CUR-28] changes on develop-v1 to develop

--- resource/js/custom/newsletter.js
+++ resource/js/custom/newsletter.js
@@ -3,31 +3,31 @@ let subscriberList = new Map();
 
 
 //Select All Function ----------------------------------------------------
-$(function () {
-    "use strict";
+// $(function () {
+//     "use strict";
 
-    //Enable iCheck plugin for checkboxes
-    //iCheck for checkbox and radio inputs
-    $('.mailbox-messages input[type="checkbox"]').iCheck({
-      checkboxClass: 'icheckbox_flat-blue',
-      radioClass: 'iradio_flat-blue'
-    });
+//     //Enable iCheck plugin for checkboxes
+//     //iCheck for checkbox and radio inputs
+//     $('.mailbox-messages input[type="checkbox"]').iCheck({
+//       checkboxClass: 'icheckbox_flat-blue',
+//       radioClass: 'iradio_flat-blue'
+//     });
 
-    //Enable check and uncheck all functionality
-    $(".checkbox-toggle").click(function () {
-      var clicks = $(this).data('clicks');
-      if (clicks) {
-        //Uncheck all checkboxes
-        $(".mailbox-messages input[type='checkbox']").iCheck("uncheck");
-        $(".ion", this).removeClass("ion-android-checkbox-outline").addClass('ion-android-checkbox-outline-blank');
-      } else {
-        //Check all checkboxes
-        $(".mailbox-messages input[type='checkbox']").iCheck("check");
-        $(".ion", this).removeClass("ion-android-checkbox-outline-blank").addClass('ion-android-checkbox-outline');
-      }
-      $(this).data("clicks", !clicks);
-    });
-}); // End of use strict
+//     //Enable check and uncheck all functionality
+//     $(".checkbox-toggle").click(function () {
+//       var clicks = $(this).data('clicks');
+//       if (clicks) {
+//         //Uncheck all checkboxes
+//         $(".mailbox-messages input[type='checkbox']").iCheck("uncheck");
+//         $(".ion", this).removeClass("ion-android-checkbox-outline").addClass('ion-android-checkbox-outline-blank');
+//       } else {
+//         //Check all checkboxes
+//         $(".mailbox-messages input[type='checkbox']").iCheck("check");
+//         $(".ion", this).removeClass("ion-android-checkbox-outline-blank").addClass('ion-android-checkbox-outline');
+//       }
+//       $(this).data("clicks", !clicks);
+//     });
+// }); // End of use strict
 
 
 
