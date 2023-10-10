3d9001de383e8442a3e5671dda4140744c5eeab5
dipalirahane <soulsoft.soul120@gmail.com>
2023-10-10 17:07:01 +0530

    changes

--- resource/js/custom/employeeHeaderConfiguration.js
+++ resource/js/custom/employeeHeaderConfiguration.js
@@ -18,6 +18,22 @@ $('#userImageMd').attr('src',profileImage);
 $('#userName').html(empdetails.name);
 $('#userIdforAvatar').html(empdetails.userid);
 
+$(window).on('beforeunload', function() {
+    // Perform logout operations here
+    // For example, make an AJAX call to a logout API endpoint
+    
+    // This is just an example, replace it with your actual logout logic
+    $.ajax({
+        url: ebase_url + 'employeeLogout', // Replace with your logout API endpoint
+        method: 'POST',
+        async: false, // Make the call synchronous to ensure it completes before the tab closes
+        success: function(response) {
+            // Handle the logout response if needed
+        }
+    });
+});
+
+
 
 
 //import employeeLogout script
