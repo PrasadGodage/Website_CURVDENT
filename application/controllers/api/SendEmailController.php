b585b9d176f776fdc7128b28156eeadb0862c19c
pradyumnSoulsoft <pradyumn.soulsoftinfotech@gmail.com>
2023-10-11 15:04:02 +0530

    [CUR-47] Changes

--- application/controllers/api/SendEmailController.php
+++ application/controllers/api/SendEmailController.php
@@ -10,9 +10,9 @@ class SendEmailController extends REST_Controller {
     public function __construct() {
 
         parent::__construct();
-        // $this->load->library('email'); 
         $this->load->library('email');
         $this->load->helper('url');
+        $this->load->model('NewsletterModel','newsletter');
         
     }
    
@@ -30,6 +30,7 @@ class SendEmailController extends REST_Controller {
         // $data = $this->post();
 
         
+        $subscriber['email'] = $this->post('mail');
         // $sub = $this->post('name');
         
         $data['firstName'] = $this->post('fname');
@@ -103,131 +104,37 @@ class SendEmailController extends REST_Controller {
         // // $this->email->message($message);
         // $this->email->message($emailContent);
         // $this->email->set_newline("\r\n");
-        $status = $this->email->send();
+        $Mailstatus = $this->email->send();
+        if(empty($id)){
+            $status = $this->newsletter->insert_newsletter($subscriber);
+            
+            if ($Mailstatus) {
+                $response['msg'] = 'Email Send Successfully!';
+                $response['msg'] = $status;
+                $response['status'] = 200;
+                $this->response($response, REST_Controller::HTTP_OK);
+            }else {
+                $response['msg'] = 'Bad Request!';
+                $response['status'] = 400;
+                $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+            }
 
-        if ($status) {
-            $response['msg'] = 'Email Send Successfully!';
-            $response['status'] = 200;
-            $this->response($response, REST_Controller::HTTP_OK);
-        }else {
-            $response['msg'] = 'Bad Request!';
-            $response['status'] = 400;
-            $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
-          }
-        // $this->email->attach($file_data['full_path']);
+        }else{
+            $newsletter_id = $this->newsletter->insert_newsletter($subscriber);
+            
+            if ($status) {
+                $response['msg'] = 'Email Send Successfully!';
+                $response['status'] = 200;
+                $this->response($response, REST_Controller::HTTP_OK);
+            }else {
+                $response['msg'] = 'Bad Request!';
+                $response['status'] = 400;
+                $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+            }
+
+        }
         
-        // if($this->email->send()){
-        //     if(delete_files($file_data['file_path']))
-        //     {
-        //         $this->session->set_flashdata('message', 'Application Sended');
-                
-        //     }
-        // }
     }
 
-    // public function upoload_file(){
 
-        //     $config['upload_path']   = './uploads/'; // Same as in the config file
-        //     $config['allowed_types'] = 'pdf';
-        //     $this->load->library('upload', $config);
-
-        //      if ($this->upload->do_upload('PDF')) {
-        //         // Upload successful, you can do further processing here
-        //         $data = $this->upload->data();
-        // }
-    
-    // }
-
-
-
-    public function send() {  
-        
-        $response = [];
-        
-        $name = $this->input->post("fname");
-        $email = $this->input->post("mail");
-        $mobile = $this->input->post("mobile");
-        $subject = $this->input->post("subject");
-        $msg = $this->input->post("msg");
-        
-        $emailContent = '
-                    <h3 align="center">Client Details</h3>
-                        <table border="1" width="100%" cellpadding="5">
-                            <tr>
-                            <td width="30%">Name</td>
-                            <td width="70%">'.$name.'</td>
-                            </tr>
-                            
-                            <tr>
-                            <td width="30%">Email Address</td>
-                            <td width="70%">'.$email.'</td>
-                            </tr>
-                            
-                            <tr>
-                            <td width="30%">Phone Number</td>
-                            <td width="70%">'.$mobile.'</td>
-                            </tr>
-                            
-                            <tr>
-                            <td width="30%">Message</td>
-                            <td width="70%">'.$msg.'</td>
-                            </tr>
-                        </table>
-                    ';
-
-
-        // $name = $this->input->post('name');
-        // $email = $this->input->post('email');
-        // $phone = $this->input->post('phone');
-        // $subject = $this->input->post('subject');
-        // $message = $this->input->post('message');
-        
-        // $emailContent = 'Phone Number: ' . $phone . "\n\n" . 'Message: ' . $message;
-        // $subject = " NewsLetter send ";
-        // $this->input->post("title");
-        // 
-        // $file_data = $this->upoload_file();
-        
-        $config=array(
-            
-            'protocol'   =>   'sendmail',
-            'smtp_host'   =>   'ssl://smtp.gmail.com',
-            'smtp_port'   =>   465,
-            'smtp_user'   =>   'pradyumnb.297@gmail.com',
-            'smtp_pass'   =>   'Pradyumn@1998',
-            'mailtype'   =>   'html',
-            'charset'   =>   'utf-8',
-            'wordwrap'   =>   TRUE
-            
-        );
-        
-        $this->email->initialize($config);
-
-        $this->email->set_newline("\r\n");
-
-        $this->email->from('pradyumnb.297@gmail.com');
-        $this->email->to('pradyumnb.297@gmail.com');
-        $this->email->subject($subject);
-        
-        $this->email->message($emailContent);
-        
-        if (!$this->email->send()) {
-            $response['msg'] = 'Email Send Successfully!';
-            $response['status'] = 200;
-            $this->response($response, REST_Controller::HTTP_OK);
-        }else {
-            $response['msg'] = 'Bad Request!';
-            $response['status'] = 400;
-            $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
-          }
-        // $this->email->attach($file_data['full_path']);
-        
-        // if($this->email->send()){
-        //     if(delete_files($file_data['file_path']))
-        //     {
-        //         $this->session->set_flashdata('message', 'Application Sended');
-                
-        //     }
-        // }
-    }
 }
