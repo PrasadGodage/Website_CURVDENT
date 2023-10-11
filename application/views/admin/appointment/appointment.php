68d8fd8b617cf3241e7d4d870563b2ada61cf34e
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-10-11 14:33:58 +0530

    [CUR-41] changes on  develop-v1 to develop

--- application/views/admin/appointment/appointment.php
+++ application/views/admin/appointment/appointment.php
@@ -36,8 +36,8 @@
                             <button type="button" class="col-md-3 btn btn-primary ml-2" id="addAppointmentBtn"><i class="fa fa-plus" aria-hidden="true"></i> New Appointment</button>
                             <!-- <button id="date" class="btn btn-xs btn-primary">Select Date -->
                             <!-- <div class=" box-controls pull-right"> -->
-                            <p>Date:<input type="text" class="col-md-3 form-control mr-2" id="datepicker" name="datepicker" placeholder="MM/DD/YYYY">
-                               </p> 
+                            <input type="text" class="col-md-3 form-control mr-2" id="datepicker" name="datepicker">
+                           
                                <!-- <p>Date: <input type="text" id="datepicker"> -->
                             <!-- </button> -->
                             <!-- </div> -->
