469575e6fd378b057fb180141d17e126a4ed174f
dipalirahane <soulsoft.soul120@gmail.com>
2023-10-12 17:56:13 +0530

    [CUR-41] changes appointment.js

--- resource/js/custom/appointment.js
+++ resource/js/custom/appointment.js
@@ -165,24 +165,85 @@ $('#addAppointmentForm').on('submit', function (e) {
 
     // Get the time value from the time input field
     var appointmentTime = $("#time").val();
+    var selectedDate = $("#date").val();
 
-//     // Split the time into hours and minutes
-var parts = appointmentTime.split(":");
-var hours = parseInt(parts[0]);
-var minutes = parseInt(parts[1]);
+    var selectedAppointmentDate = selectedDate.split('/').join('-');
 
-// Determine AM or PM
-var ampm = hours >= 12 ? "PM" : "AM";
+    var parts = selectedAppointmentDate.split("-"); // Split the input into an array
 
-// Convert to 12-hour format
-if (hours > 12) {
-    hours -= 12;
-} else if (hours === 0) {
-    hours = 12;
+    var formattedDate2 = parts.reverse().join("-");
+    // Split the input date using the hyphen as a separator
+       if (parts.length === 3) {
+       var dateComponents1 = formattedDate2.split('-');
+
+       // Rearrange the components into the "yyyy-mm-dd" format
+       var formattedDate3 = dateComponents1[0] + '-' + dateComponents1[2] + '-' + dateComponents1[1];
+
 }
 
-// Create the formatted time string
-var formattedTime = hours + ":" + minutes + " " + ampm;
+         //     // Split the time into hours and minutes
+         var parts = appointmentTime.split(":");
+         var hours = parseInt(parts[0]);
+         var minutes = parseInt(parts[1]);
+
+        // Determine AM or PM
+        var ampm = hours >= 12 ? "PM" : "AM";
+
+        // Convert to 12-hour format
+        if (hours > 12) {
+           hours -= 12;
+        } else if (hours === 0) {
+           hours = 12;
+        }
+
+        var formattedTime = hours + ":" + minutes + " " + ampm;
+        // Create the formatted time string
+
+
+        
+        //Logic for appointment timing selection 
+$.ajax({
+
+    url: ebase_url+'appointment_api',
+
+    type: 'GET',
+
+    async:false,
+
+    headers: {
+        "Authorization": etoken
+    },
+
+    dataType: 'json',
+
+    success: function (response) {
+    
+
+        if (response.status == 200) {
+
+            if (response.data.length != 0) {
+                for (var i = 0; i < response.data.length; i++) {
+
+                    if(response.data[i].date === formattedDate3){
+                        if(response.data[i].time === formattedTime){
+
+
+                            alert("This time is alredy book please choose other time");
+                        }
+
+                      // appointmentList1.set(response.data[i].id, response.data[i]);
+
+                    }                                              
+                                       
+                }
+             }
+          }
+
+    }
+    
+});
+
+
     // Add the time value to the FormData object
     formdata.append('time', appointmentTime);
 
