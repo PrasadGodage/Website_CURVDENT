8c4b0ee195af4b06df1b02b63466fd68126634fd
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-10-14 12:56:52 +0530

    [CUR-58] changes on  develop-v1 to develop

--- application/views/ui/home/home.php
+++ application/views/ui/home/home.php
@@ -597,65 +597,61 @@
 <!-- Appointment Form -->
 <section class="section-padding" data-scroll-index="5" data-background="<?php echo base_url() . 'uiAssets/img/slider/1.jpg'; ?>">
     <div class="container">
-		<div class="row">
+        <div class="row">
             <div class="col-md-12 mb-30">
                 <h2 class="section-title" id="contact">Contact Us</h2>
-                    <hr class="line line-hr-left text-center">
+                <hr class="line line-hr-left text-center">
             </div>
         </div>
- 	    <div class="row">
+        <div class="row">
             <!-- Contact Info -->
-            <div class="col-md-3 animate-box" data-animate-effect="fadeInUp"></div>
+            <div class="col-md-2 animate-box" data-animate-effect="fadeInUp"></div>
 
-            <div class="col-md-6 app-sub-w3 animate-box" data-animate-effect="fadeInUp">
-                            <br>
+            <div class="col-md-8 app-sub-w3 animate-box" data-animate-effect="fadeInUp">
+                <br>
                 <form id="send-form" role="form" method="post">
                     <input type="hidden" class="form-control" id="id1" name="id">
 
- 				        <div class="row">
-                            <div class="col-12">
-                                <div class="alert alert-success contact__msg" id="alertMsg" style="display: none" role="alert">
-                                	<span class="text-danger">Your message was sent successfully.</span>
-                                </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="alert alert-success contact__msg" id="alertMsg" style="display: none" role="alert">
+                                <span class="text-danger">Your message was sent successfully.</span>
                             </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12 form-group">
+                            <input name="fullName" type="text" id="fullName" placeholder="Enter Name *" required>
                         </div>
 
-                        <!-- <div class="form-group row"> -->
-                        <div class="row">
-                                 
-                            <div class="col-md-12 form-group">
-                                <input name="fullName" type="text" id="fullName" placeholder="Enter Name *" required>
-                            </div>
-                              
-                            <div class="col-md-6 form-group">
-                                <input name="email" type="email" id="email" placeholder="Email *" maxlength="10" required>
-                            </div>
-                            <div class="col-md-6 form-group">
-                                <input name="contactNo" type="text" id="contactNo" placeholder="Enter Contact No *" required>
-                            </div>
-                            <div class="col-md-6 form-group">
-                                <input name="date" type="date" id="date" required>
-                            </div>
-                            <div class="col-md-6 form-group">
-                                <input name="time" type="time" id="time" required>
-                            </div>
-                            <div class="col-md-12 form-group">
-                                <input name="address" type="text" id="address" cols="30" placeholder="Enter Address *" required>
-                            </div>
-                            <div class="col-md-12">
-                                <input name="submit" type="submit" id="appointment" name="appointment" value="BOOK APPOINTMENT">
-                            </div>
+                        <div class="col-md-6 form-group">
+                            <input name="email" type="email" id="email" placeholder="Email *" maxlength="10" required>
                         </div>
+                        <div class="col-md-6 form-group">
+                            <input name="contactNo" type="text" id="contactNo" placeholder="Enter Contact No *" required>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <input name="date" type="date" id="date" required>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <input name="time" type="time" id="time" required>
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <input name="address" type="text" id="address" cols="30" placeholder="Enter Address *" required>
+                        </div>
+                        <div class="col-md-12">
+                            <input name="submit" type="submit" id="appointment" name="appointment" value="BOOK APPOINTMENT">
+                        </div>
+                    </div>
                 </form>
             </div>
-     
- 		    <div class="col-md-3 animate-box" data-animate-effect="fadeInUp"></div>
-     
-	    </div>
-     
-    </div>
 
+            <div class="col-md-2 animate-box" data-animate-effect="fadeInUp"></div>
+        </div>
+    </div>
 </section>
+
 
 
 
