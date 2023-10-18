746d6e134e9d5625c33e7fa3c0e3cdaf634c0b64
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-10-18 13:20:09 +0530

    [CUR-28] changes on develop-v1 to develop

--- application/views/ui/home/home.php
+++ application/views/ui/home/home.php
@@ -670,10 +670,10 @@
 
 
     <!-- NewsLetters -->
-    <section class="section-padding mb-20 shadow" data-scroll-index="" >
+    <section class="section-padding shadow" data-scroll-index="" >
         <div class="container">
             <div class="row">
-                <div class="col-md-12 mb-30">
+                <div class="col-md-12 mb-20">
                     <h2 class="section-title" id="contact">Newsletter</h2>
                     <hr class="line line-hr-left text-center">
                 </div>
@@ -690,13 +690,14 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-1 form-group"></div>
+                            <div class="col-md-2 form-group"></div>
                             <div class="col-md-6 form-group">
                                 <input name="email" type="email" id="email" placeholder="Email *" required>
                             </div>
                             <div class="col-md-2">
                                 <input name="submit" type="submit" value="Subscribe">
                             </div>
+                            <div class="col-md-2 form-group"></div>
                         </div>
                     </form>
                 </div>
